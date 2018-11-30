dc6755b906ecf85586d086cc04491698704da983
Andrew MacLeod <andrewmorrismacleod@googlemail.com>
2018-11-30 17:20:16 +0000

    Passing Karaoke Class

---
+++ karaoke.rb
@@ -0,0 +1,10 @@
+class Karaoke
+
+  attr_reader :rooms, :till
+
+  def initialize(rooms, till)
+    @rooms = rooms
+    @till = till
+  end
+
+end
--- specs/karaoke_specs.rb
+++ specs/karaoke_specs.rb
@@ -1,6 +1,9 @@
 require('minitest/autorun')
 require('minitest/rg')
 require_relative('../karaoke')
+require_relative('../room')
+require_relative('../guest')
+require_relative('../song')
 
 class TestKaraoke < MiniTest::Test
 
@@ -23,10 +26,10 @@ class TestKaraoke < MiniTest::Test
     @rooms = [@room1, @room2, @room3]
 
     @guest1 = Guest.new("Steve", 100)
-    @guest1 = Guest.new("Sarah", 50)
-    @guest1 = Guest.new("John", 10)
-    @guest1 = Guest.new("Bruce", 5)
-    @guest1 = Guest.new("Lars", 1)
+    @guest2 = Guest.new("Sarah", 50)
+    @guest3 = Guest.new("John", 10)
+    @guest4 = Guest.new("Bruce", 5)
+    @guest5 = Guest.new("Lars", 1)
 
     @guests = [@guest1, @guest2, @guest3, @guest4, @guest5]
 
@@ -34,12 +37,16 @@ class TestKaraoke < MiniTest::Test
 
   end
 
-  def test_guest_name
-    assert_equal('Steve', @guest.name)
+  def test_karaoke_rooms__room2_capacity
+    assert_equal(7, @karaoke.rooms[1].capacity)
   end
 
-  def test_guest_wallet
-    assert_equal(100, @guest.wallet)
+  def test_karaoke_rooms__room3_song2_artist
+    assert_equal('Metallica', @karaoke.rooms[2].songs['Nothing Else Matters'].artist)
+  end
+
+  def test_karaoke_till
+    assert_equal(200, @karaoke.till)
   end
 
 
